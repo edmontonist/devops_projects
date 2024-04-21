946318552fae8ecdd9f77b0cba3637ce3d59d5f4
edmontonist <142277296+edmontonist@users.noreply.github.com>
2024-04-21 17:40:19 -0600

    Add files via upload

---
+++ 202-Terraform-Phonebook-Application-deployed-on-AWS/Tf-Solution Files/variable.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "XXXXXXXXXXXXX" # Your github name
+}
+
+variable "git-token" {
+  default = "XXXXXXXXXXXX"  # Your git token
+}
+
+variable "key-name" {
+  default = "XXXXXX" # your key name without .pem extension
+}
+
+variable "hosted-zone" {
+    default = "XXXXXXXXXXX" # Your hosted zone
+}
